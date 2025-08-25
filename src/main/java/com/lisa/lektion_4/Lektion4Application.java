79ad4c29b3e0f34160db37000919442bcd8fd8ff
liluppis <lisa.upp@hotmail.com>
2025-08-25 10:34:21 +0200

    initial commit

---
+++ src/main/java/com/lisa/lektion_4/Lektion4Application.java
@@ -0,0 +1,13 @@
+package com.lisa.lektion_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lektion4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lektion4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lisa/lektion_4/Lektion4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisa.lektion_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lektion4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
